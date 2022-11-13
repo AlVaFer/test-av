487e84432766772b8dcc9c9b63bcca81398301c5
Al Tfe <heroku7@gmx.es>
2022-11-13 21:41:53 +0000

    podcasts page

---
+++ src/app/pages/Podcasts/index.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect, useMemo } from 'react'
+import { useDispatch } from 'react-redux'
+
+import usePodcasts from '../../components/Podcasts/usePodcasts'
+import PodcastsListItem from '../../components/Podcasts/PodcastsListItem'
+import PodcastsSearchBar from '../../components/Podcasts/PodcastsSearchBar'
+import { setLoading } from '../../store/features/loading'
+
+import './styles.scss'
+
+export const Podcasts = () => {
+  const dispatch = useDispatch()
+  const { data = [], isLoading } = usePodcasts()
+  const [filteredPodcasts, setFilteredPodcasts] = useState([])
+
+  useEffect(() => {
+    dispatch(setLoading(isLoading))
+  }, [isLoading, dispatch])
+
+  return (
+    <div className="podcasts">
+      <div className="podcasts__searchbar">
+        <PodcastsSearchBar
+          data={data ?? []}
+          setFilteredPodcasts={setFilteredPodcasts}
+          filteredPodcasts={filteredPodcasts}
+        />
+      </div>
+      <div className="podcasts__list">
+        {filteredPodcasts?.map((podcast) => (
+          <PodcastsListItem podcast={podcast} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Podcasts
