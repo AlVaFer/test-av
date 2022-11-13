8abdaf4bb4bb3a7973abd86cd3131a1dcf4142e8
Al Tfe <heroku7@gmx.es>
2022-11-13 21:37:47 +0000

    routers

---
+++ src/app/routers/PodcasterRouter.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import Podcast from '../pages/Podcast'
+import Podcasts from '../pages/Podcasts'
+import PodcastEpisode from '../pages/PodcastEpisode'
+
+export const PodcasterRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Podcasts />} />
+      <Route path="/podcast/:podcastId" element={<Podcast />} />
+      <Route
+        path="/podcast/:podcastId/episode/:episodeId"
+        element={<PodcastEpisode />}
+      />
+    </Routes>
+  )
+}
+
+export default PodcasterRouter
